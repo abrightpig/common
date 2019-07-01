8b8a211def5b33ddec241a4bc2bbb48721952c18
liangzhu <brightpig61@gmail.com>
2019-07-01 16:05:20 +0800

    update MicroSecord

--- cpp/get_second.cc
+++ cpp/get_second.cc
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <stdint.h>
 
-uint64_t GetSecond() {
+uint64_t MicroSecond() {
     struct    timeval tm;
     gettimeofday(&tm, NULL);
     return (tm.tv_sec * 1000000 + tm.tv_usec) / 100;
 }
 
 int main(void) {
-    printf("second is %lu\n", GetSecond());
+    printf("second is %lu\n", MicroSecond());
     return 0;
 }
